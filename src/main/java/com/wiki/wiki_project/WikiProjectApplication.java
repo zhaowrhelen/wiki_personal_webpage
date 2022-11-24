a2d72f4f09777d7b0fe8b6d3014ab9b7dcc1382c
zhaowrhelen <58273143+zhaowrhelen@users.noreply.github.com>
2022-11-23 16:25:05 -0800

    first version

---
+++ src/main/java/com/wiki/wiki_project/WikiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wiki.wiki_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wiki/wiki_project/WikiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiki.wiki_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
